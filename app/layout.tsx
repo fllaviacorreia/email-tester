f37ed61542c9b0f48429f5683a614aef510dae36
Flávia Correia <fllaviacorreia@outlook.com>
2026-02-13 09:43:15 -0300

    update color

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <small className="absolute bottom-2 text-xs opacity-50">Repositório no <a href="https://github.com/fllaviacorreia/email-tester" target="_blank" rel="noopener noreferrer">GitHub</a></small>
+        <small className="absolute bottom-2 text-xs opacity-70">Repositório no <a href="https://github.com/fllaviacorreia/email-tester" target="_blank" rel="noopener noreferrer">GitHub</a></small>
       </body>
     </html>
   );
